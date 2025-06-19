8dc3df0634f24fa6f0884d74bf14a7e807c10e22
maulik <maulikgarg678@gmail.com>
2025-06-19 11:19:19 +0530

    add room data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
 # Core game logic such as starting a new game and moving 
+
+import rooms
+
+print("Welcome to the game. Press any key to generate a new room.\n>")
+while(input(">")):
+  rooms.printRoom()
--- src/rooms.py
+++ src/rooms.py
@@ -1,3 +1,30 @@
 # This file is responsible for all code related to room generation.
 
+from pathlib import Path
 import random 
+import yaml
+
+# get template file path
+srcDir = Path(__file__).resolve().parent
+templatePath = srcDir.parent / "data" / "room_template.yaml"
+
+# load the template room file
+with open(templatePath, "r") as templateFile:
+  templates = yaml.safe_load(templateFile)
+
+def generateRoom():
+  light = random.choice(templates["lightLevels"])
+  size =  random.choice(templates["sizeLevels"])
+  return light, size
+
+def printRoom(light = None , size = None):
+  roomLine = random.choice(templates["roomSpawnLine"])
+
+  # if the lighting or size is not provided for the room,
+  # generate it randomly.
+  if not (light) or not (size):
+    light, size = generateRoom()
+  
+  # format the line to be printed
+  roomLineToPrint = roomLine.format(selectedlighting = light, selectedSize = size)
+  print(roomLineToPrint)
